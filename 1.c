3fe7e4cd1d54ddc4e1746fa7cfd1dfa4fddc9da2
dhruviklathiya <115994798+dhruviklathiya@users.noreply.github.com>
2023-02-21 00:14:13 +0530

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main (){
     printf("\t \t \t * \t \t * \n\n");
-	printf("\t \t *\t \t * \n\n");
-	printf("\t * \t \t * \n\n");
-	printf("\t \t *\t \t * \n\n");
-	printf("\t \t \t * \t \t * \n\n");
+    printf("\t \t *\t \t * \n\n");
+    printf("\t * \t \t * \n\n");
+    printf("\t \t *\t \t * \n\n");
+    printf("\t \t \t * \t \t * \n\n");
     printf("\n\n\n\n\n\n\n\n\n");
     printf("\t*\t*\t*\t*\t*\t*\t*\n\n\n\n");
     printf("*\t*\t*\t*\t*\t*\t*\t*\n\n\n");
